775fc9024183760982deced216000cc070413129
Agustin F. Ruiz <62730262+darksoul96@users.noreply.github.com>
2024-07-13 23:28:41 -0300

    Added some spaces and comments

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import ttkbootstrap as ttk
 import ttkbootstrap.constants as constants
 
 
+# Method called after submitting
 def SaveExpense():
     try:
         # expense_int_value = int(expense_entry.get())
@@ -28,9 +29,9 @@ if __name__ == "__main__":
     )
     title_label.pack(pady=15)
 
+    # Main input fram
     frame = ttk.Frame(master=window)
     expense_label = ttk.Label(master=frame, text="Enter expense: ", padding=10)
-    # expense_int_value = tk.IntVar()
     expense_entry = ttk.Entry(master=frame, width=50)
 
     save_button = ttk.Button(master=frame, text="Save", padding=5, command=SaveExpense)
@@ -38,16 +39,20 @@ if __name__ == "__main__":
         master=frame, text="Quit", command=window.destroy, padding=5
     )
 
+    # Add labels, input and buttons to the window
     expense_label.pack()
     expense_entry.pack(pady=(20))
     save_button.pack(side=tk.LEFT, pady=30, padx=30)
     quit_button.pack(side=tk.RIGHT, padx=20)
     frame.pack()
 
+    # This label shows whether the input was correct or not
     output_string = tk.StringVar()
     output_label = ttk.Label(master=window, textvariable=output_string)
     output_label.pack()
 
+    # This line makes sure that the cursor starts at the input
     expense_entry.focus()
+
     # run window
     window.mainloop()
